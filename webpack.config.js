ef3d8bd27acf408b776363bcab231ea482e5080c
SeerjUeo <sergiu.gata@emanuel.ro>
2020-02-05 11:12:05 +0200

    add menu

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,39 @@ module.exports = {
     rules: [{
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: "file-loader",
+        options: {
+          publicPath: "build/"
+        }
+      },
+      {
+        loader: "image-webpack-loader",
+        options: {
+          mozjpeg: {
+            progressive: false,
+            quality: 65
+          },
+          optipng: {
+            enabled: false
+          },
+          pngquant: {
+            quality: "65-90",
+            speed: 4
+          },
+          gifsicle: {
+            interlaced: false
+          }
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        loader: "file-loader",
+        options: {
+          publicPath: "build/"
+        }
       }
     ]
   }
